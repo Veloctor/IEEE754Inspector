e5b2e534b51f9bb3f848d736a0d2f9789299eca1
Velctor <Velctor@qq.com>
2023-02-04 20:54:41 +0800

    初始化

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace IEEE754Calculator
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            RefreshDisplay(0);
+            ShowMsg("预计未来加入基本初等函数计算\\双精度等");
+        }
+
+        void RefreshDisplay(int bits)
+        {
+            (int sign, int expo, int mantissa)  = MathTool.SplitBinary32Bits(bits);
+            RealValueBox.Text = MathTool.As<int, float>(bits).ToString();
+            SignBitBox.Text = MathTool.IntToBinString(sign, 1);
+            ExponentBitBox.Text = MathTool.IntToBinString(expo, 8);
+            MantissaBitBox.Text = MathTool.IntToBinString(mantissa, 23);
+            //refresh details
+            bool isDenormal = expo == 0 && mantissa != 0;
+            int mantValBits = MathTool.SetupBinary32Bits(0, 127, mantissa);
+            float mantVal = MathTool.As<int, float>(mantValBits);
+            if (expo == 0) mantVal--;
+            MantissaValBox.Text = mantVal.ToString();
+            ExponentValBox.Text = (expo - 127).ToString();
+            SignValBox.Text = sign == 0 ? "+" : "-";
+            IsNormalLabel.Content = isDenormal ? "是" : "否";
+
+            ShowMsg($"0x{bits:X}\n0b{MathTool.IntToBinString(bits, 32)}");
+        }
+
+        void ShowMsg(string msg) => MessagesBox.Text = msg;
+
+        void BitBoxesKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                string txt = SignBitBox.Text.TrimStart('0');
+                var result = MathTool.TryParseBin(txt, out int signBit, 1);
+                if (result != ParseResult.成功)
+                {
+                    ShowMsg($"{txt}不能转换为符号位:{result}");
+                    return;
+                }
+                txt = ExponentBitBox.Text.TrimStart('0');
+                result = MathTool.TryParseBin(txt, out int exponentBits, 8);
+                if (result != ParseResult.成功)
+                {
+                    ShowMsg($"{txt}不能转换为指数位:{result}");
+                    return;
+                }
+                txt = MantissaBitBox.Text.TrimStart('0');
+                result = MathTool.TryParseBin(txt, out int mantissaBits, 23);
+                if (result != ParseResult.成功)
+                {
+                    ShowMsg($"{txt}不能转换为尾数位:{result}");
+                    return;
+                }
+                int bits = MathTool.SetupBinary32Bits(signBit, exponentBits, mantissaBits);
+                RefreshDisplay(bits);
+            }
+        }
+
+        void RealValueBoxKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                string txt = RealValueBox.Text.TrimStart('0');
+                if (float.TryParse(txt, out float result))
+                    RefreshDisplay(MathTool.As<float, int>(result));
+                else
+                    ShowMsg($"{txt}不能转换为单精度浮点");
+            }
+        }
+    }
+}
---
+++ MathTool.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+
+namespace IEEE754Calculator
+{
+    public enum ParseResult
+    {
+        成功,
+        字符串过长,
+        有非法字符
+    }
+    internal static class MathTool
+    {
+        public static (int sign, int expo, int mantissa) SplitBinary32Bits(int floatBits) => 
+            (sign: floatBits >> 31, 
+            expo: (floatBits & int.MaxValue) >> 23, 
+            mantissa: floatBits & 0b111_11111_11111_11111_11111);
+
+        public static int SetupBinary32Bits(int signBit, int exponentBit, int mantissaBit)
+        {
+            exponentBit &= byte.MaxValue;
+            mantissaBit &= 0b111_11111_11111_11111_11111;
+            int allBits = (signBit << 31) | (exponentBit << 23) | (mantissaBit);
+            return allBits;
+        }
+
+        public static unsafe Tto As<Tfrom, Tto>(Tfrom from) where Tfrom : unmanaged where Tto : unmanaged => *(Tto*)&from;
+
+        public static ParseResult TryParseBin(string binStr, out int result, int lengthLimit)
+        {
+            result = 0;
+            if (binStr.Length > lengthLimit)
+                return ParseResult.字符串过长;
+            int bin = 0;
+            for (int i = 0; i < binStr.Length; i++)
+            {
+                bin <<= 1;
+                if (TryParseBinChar(binStr[i], out int val))
+                    bin |= val;
+                else
+                    return ParseResult.有非法字符;
+            }
+            result = bin;
+            return ParseResult.成功;
+        }
+        public static bool TryParseBinChar(char c, out int result)
+        {
+            result = c - '0';
+            return result == (result & 1);
+        }
+        public static string IntToBinString(int x, int length) => IntToBinString((uint)x, length);
+        public static string IntToBinString(uint x, int length)
+        {
+            var sb = new StringBuilder();
+            while (length--> 0)
+            {
+                sb.Insert(0, (char)((x & 1) + '0'));
+                x >>= 1;
+            }
+            return sb.ToString();
+        }
+    }
+}
